44d88b0ebd9e4733ece80441615564585dfb470c
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2022-12-26 11:40:57 +0700

    update author and day

--- config/src/main/java/com/hcmute/ecom/config/FilterConfig.java
+++ config/src/main/java/com/hcmute/ecom/config/FilterConfig.java
@@ -5,6 +5,11 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author Nhat Phi
+ * @since 2022-11-22
+ * This class use to log request to terminal
+ * */
 @Configuration
 public class FilterConfig {
     @Bean
